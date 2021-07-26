185c5ce55be785feff7ca0d22c1ca4e3740edadc
vinidotruan <contatoruanvinicius@gmail.com>
2021-07-26 17:28:54 -0300

    - Pequenos ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ArticleComponent } from './article/article.component';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ArticleCardComponent, ArticleComponent],
@@ -23,6 +25,12 @@ import { AccordionModule } from 'ngx-bootstrap/accordion';
     BrowserAnimationsModule,
     PaginationModule.forRoot(),
     AccordionModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -9,10 +9,17 @@ export class HttpService {
   constructor(protected http: HttpClient) {}
 
   public get = (uri: string, params?: any): Observable<any> => {
-    return this.http.get(uri, { params });
+    return this.http.get(uri, { headers: this.getHeaders(), params });
   };
 
   public post = (uri: string, body?: any): Observable<any> => {
-    return this.http.post(uri, body);
+    return this.http.post(uri, body, { headers: this.getHeaders() });
   };
+
+  public getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'my-auth-token',
+    });
+  }
 }
